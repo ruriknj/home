7d99368abdfa69adf7bf878d0c8543cff9bcfe76
ruriknj <ruriknj@hotmail.com>
2020-10-11 00:07:50 -0300

    Updates

---
+++ precache-manifest.b0ffea353d6712201e3c8653ae505eef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e86fbae13ad4d96c46b54dcb0c31b43",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "5dfb4c196b29d00aaa1f",
+    "url": "/home/static/css/main.0053814e.chunk.css"
+  },
+  {
+    "revision": "e02b6b2fce09bb6f8192",
+    "url": "/home/static/js/2.11eb4a17.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/home/static/js/2.11eb4a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dfb4c196b29d00aaa1f",
+    "url": "/home/static/js/main.896d4333.chunk.js"
+  },
+  {
+    "revision": "5e6b07412105f2716fd4",
+    "url": "/home/static/js/runtime-main.1d2acfd7.js"
+  },
+  {
+    "revision": "9ea4c5675640c531a07f4525b353cf4a",
+    "url": "/home/static/media/arrow.9ea4c567.svg"
+  },
+  {
+    "revision": "3182af67b01a1865777c77725d768901",
+    "url": "/home/static/media/main-image.3182af67.svg"
+  },
+  {
+    "revision": "2849f9ddb31b5263df51dc236641942d",
+    "url": "/home/static/media/product.2849f9dd.svg"
+  }
+]);
